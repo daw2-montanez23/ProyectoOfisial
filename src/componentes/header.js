ffba19a92fe81c07f8d77f9e0cde8128582ed84e
daw2-montanez23 <113450955+daw2-montanez23@users.noreply.github.com>
2023-03-23 19:53:48 +0000

    commit-header

---
+++ src/componentes/header.js
@@ -0,0 +1,12 @@
+export const  header = {
+    template: `
+    <div class="container row d-flex">
+        <div class="col-4 text-center">Logoe</div>
+        <div class="col-4 text-center">Titulo</div>
+        <div class="col-4 text-center">Menu</div>
+    </div> 
+    `,
+    script : ()=>{
+        console.log('esto es el componente header')
+    }
+} 
